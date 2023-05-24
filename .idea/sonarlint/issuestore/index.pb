2577eb85c2023ab94470c9bcd1d0cc75d06a553c
Вадим Валыгин <vadim12388@mail.ru>
2023-05-24 16:00:55 +0300

    all

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
+8
+Makefile,8/3/836efb6e25a091dcb4ff8e1dbb2f0be6a5cbf14c
+J
+src/components/Counter.jsx,4/1/41e34f0d90edc78863b7671d01c73493902000f9
+\
+,src/components/UI/button/MyButton.module.css,f/e/fe095607c1e7d0deee5c199725d32057634d614b
--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
+import './styles/App.css';
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
+
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'aJavaScript 1', body: 'dDescription 1'},
+    {id: 2, title: 'bJavaScript 2', body: 'cDescription 2'},
+    {id: 3, title: 'cJavaScript 3', body: 'bDescription 3'},
+    {id: 4, title: 'dJavaScript 4', body: 'aDescription 4'},
+  ])
+
+  const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const sortedPosts = useMemo(() => {
+      if (selectedSort) {
+        return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      }
+      return posts;
+  }, [selectedSort, posts])
+
+  const sortedAndSearchPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedPosts])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+  }
+
   return (
     <div className="App">
-      Hello!
+      <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <MyInput
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value.toLowerCase())}
+        placeholder="Поиск..."
+      />
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue='сортировать'
+        options = {[
+          { value: 'title', name: 'по названию'},
+          { value: 'body', name: 'по описанию'}
+        ]}
+      />
+      {sortedAndSearchPosts.length
+        ?
+        <PostList remove={removePost} posts={sortedAndSearchPosts} title="Посты про JS"/>
+        :
+        <h1
+          style={{textAlign:"center"}}>
+          Посты не найдены!
+        </h1>
+      }
     </div>
   );
 }
